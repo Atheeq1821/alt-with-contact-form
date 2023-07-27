8ecb5835b8dac3bea646f0f0072448a0a8065533
Syed Atheequr Rahaman <syed21ad067@rmkcet.ac.in>
2023-07-27 12:23:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const about =document.getElementById("about")
+about.addEventListener("mouseover",function(){
+    const aboutContent = document.getElementById("about-content")
+    about.style.width="60%"
+    aboutContent.style.display="inline-block"
+})
